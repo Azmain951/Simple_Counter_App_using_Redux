f25b2adfd31e240f0565792c07f1f5d628fd4fbd
Afridi Azmain <Azmain951>
2022-07-01 13:48:35 +0600

    redux connection with react

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Counter = () => {
+    const count = useSelector(state => state.count);
+    const dispatch = useDispatch();
     return (
         <div>
             <h2>Counter App</h2>
-            <h3>Count: 0</h3>
+            <h3>Count: {count}</h3>
             <button>Increment</button>
             <button>Decrement</button>
             <br />
