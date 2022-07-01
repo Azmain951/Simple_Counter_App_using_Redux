6f8a9a69f0ee1f5d273384696bec29c56d65e613
Afridi Azmain <Azmain951>
2022-07-01 13:43:11 +0600

    redux setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
       <h2>Welcome to my Counter App</h2>
+      <Counter></Counter>
     </div>
   );
 }
---
+++ src/components/Counter.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Counter = () => {
+    return (
+        <div>
+            <h2>Counter App</h2>
+            <h3>Count: 0</h3>
+            <button>Increment</button>
+            <button>Decrement</button>
+            <br />
+            <button>Reset</button>
+        </div>
+    );
+};
+
+export default Counter;
