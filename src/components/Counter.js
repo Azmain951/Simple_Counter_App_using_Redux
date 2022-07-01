8549ee4151c00f7fa74757f03c4437955e75fd97
Afridi Azmain <Azmain951>
2022-07-01 13:53:43 +0600

    counter incremnet decrement reset functionalities added

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { decrementActions, incrementActions, resetActions } from '../services/actions/counterActions';
 
 const Counter = () => {
     const count = useSelector(state => state.count);
     const dispatch = useDispatch();
+    const handleIncrement = () => {
+        dispatch(incrementActions())
+    }
+    const handleDecrement = () => {
+        dispatch(decrementActions())
+    }
+    const handleReset = () => {
+        dispatch(resetActions())
+    }
     return (
         <div>
             <h2>Counter App</h2>
             <h3>Count: {count}</h3>
-            <button>Increment</button>
-            <button>Decrement</button>
+            <button onClick={handleIncrement} >Increment</button>
+            <button onClick={handleDecrement}>Decrement</button>
             <br />
-            <button>Reset</button>
+            <button onClick={handleReset}>Reset</button>
         </div>
     );
 };
